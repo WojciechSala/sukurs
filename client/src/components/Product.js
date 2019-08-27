a26f99d6b087cbe07621410d5cbbd344eb08382f
Wojciech Sala <wojteksala190@gmail.com>
2019-08-27 15:00:41 +0200

    products grid template

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledProduct = styled.div``;
+const StyledProduct = styled.div`
+  width: 150px;
+  height: 200px;
+  max-width: calc((100vw - 36px) / 2);
+  background-color: blue;
+
+  :nth-child(odd) {
+    justify-self: start;
+  }
+  :nth-child(even) {
+    justify-self: end;
+  }
+`;
 const StyledProductImage = styled.div``;
 const StyledProductInfo = styled.div``;
 
--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -2,10 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 import Product from './Product.js';
 
-const StyledProducts = styled.div``;
+const StyledProducts = styled.div`
+  width: calc(100vw - 32px);
+  padding: 0 16px;
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-row-gap: 50px;
+`;
 
 const Products = () => (
   <StyledProducts>
+    <Product />
+    <Product />
+    <Product />
+    <Product />
     <Product />
     <Product />
     <Product />
