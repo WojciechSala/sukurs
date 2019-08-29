5447f8514ac7d29f887003511796935b028f0b2f
Wojciech Sala <wojteksala190@gmail.com>
2019-08-29 15:13:21 +0200

    products schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import products from './routes/products';
+import keys from './config/keys';
 
 const app = express();
 
 app.get('/', (req, res) => res.send('asd'));
-
 app.listen(process.env.PORT || 3001);
+
+app.use('/products', products());
+
+mongoose
+  .connect(keys.mongodb.DB, { useNewUrlParser: true })
+  .then(console.log('Connected successfully!'))
+  .catch(err => console.error('\nCan not connect to the database\n\n' + err));
---
+++ server/src/routes/products.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { returnProducts } from '../actions/product';
+
+export default () => {
+  const api = Router();
+
+  api.get('/', (req, res) => {
+    res.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.append('Access-Control-Allow-Credentials', 'true');
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.json(returnProducts());
+  });
+
+  return api;
+};
